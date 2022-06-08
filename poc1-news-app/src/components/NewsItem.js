29827f7597e266040b256c70af93b4f67fc063d8
it-dreams <rahulraj.holi888@gmail.com>
2022-06-08 14:23:15 +0530

    Add Spinner in app

--- poc1-news-app/src/components/NewsItem.js
+++ poc1-news-app/src/components/NewsItem.js
@@ -8,11 +8,11 @@ class NewsItem extends Component {
     return (
       <div className="my-3">
         <div className="card" style={{ width: "18rem" }}>
-          <img src={imgUrl ? imgUrl : noImage} className="card-img-top" alt="..." />
+          <img src={imgUrl ? imgUrl : noImage} className="card-img-top" alt="..."  style={{height: '200px'}}/>
           <div className="card-body text-start">
             <h5 className="card-title">{title}...</h5>
             <p className="card-text">{description}...</p>
-            <a href={newsUrl} target="_blank" className="btn btn-sm btn-primary">Read More</a>
+            <a href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Read More...</a>
           </div>
         </div>
       </div>
