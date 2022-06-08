fd7f2e80a2ef1b4842d0a928b046e709b1b93cad
it-dreams <rahulraj.holi888@gmail.com>
2022-06-08 14:23:51 +0530

    Add Spinner in app

--- poc1-news-app/src/App.js
+++ poc1-news-app/src/App.js
@@ -6,18 +6,20 @@ import News from './components/News';
 function App() {
   const apiKey = process.env.REACT_APP_NEWS_API;
   const newsType = 'everything';
-  const newsDomain = 'aajtak.in';
+  const newsDomain = 'aajtak.in'; //abplive.com, indiatoday.in, indiatvnews.com, 
   const country = 'in';
   const category = 'business';
-  const pageSize = '10';
+  const pageSize = '30';
 
   return (
     <div className="App">
       <Navbar />
-      {/* <News style={{ minHeight: '90vh' }} apiKey={apiKey} newsType={newsType} newsDomain={newsDomain} /> */}
-      <News style={{ minHeight: '90vh' }} apiKey={apiKey} newsType={newsType} newsDomain={newsDomain} category={category} country={country} pageSize={pageSize} />
+      <div style={{ minHeight: '85vh' }}>
+        {/* <News style={{ minHeight: '90vh' }} apiKey={apiKey} newsType={newsType} newsDomain={newsDomain} /> */}
+        <News style={{ minHeight: '90vh' }} apiKey={apiKey} newsType={newsType} newsDomain={newsDomain} category={category} country={country} pageSize={pageSize} />
+      </div>
       <Footer />
-    </div>
+    </div >
   );
 }
 
---
+++ poc1-news-app/src/components/Spinner.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import loading from '../assest/images/loading1.gif';
+
+function Spinner() {
+    return (
+        <div>
+            <img src={loading} alt="loading" />
+        </div>
+    )
+}
+
+export default Spinner
