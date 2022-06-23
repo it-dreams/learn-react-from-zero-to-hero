59cdefd256807097459860e2bbc8eeefdf3654bf
it-dreams <rahulraj.holi888@gmail.com>
2022-06-23 10:54:39 +0530

    updated page not found

--- poc1-news-app/src/components/PageNotFound.js
+++ poc1-news-app/src/components/PageNotFound.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 function PageNotFound() {
+    const PnfStyle = {
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: '20px',
+        height: '85vh'
+    };
+
     return (
-        <div>
-            Page Not Found!
+        <div style={PnfStyle}>
+            <h1 style={{fontSize: '80px', fontWeight: 'bold', color: 'orange'}}>404</h1>
+            <p>Oops... Page is not found!</p>
+            <Link to='/'>Back to Home</Link>
         </div>
     )
 }
