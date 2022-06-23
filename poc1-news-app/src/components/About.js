087b08b945f03548745d918c4617d70d415642b8
it-dreams <rahulraj.holi888@gmail.com>
2022-06-23 10:59:21 +0530

    Updted categories & about page

--- poc1-news-app/src/components/About.js
+++ poc1-news-app/src/components/About.js
@@ -2,8 +2,9 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>
-        About Page
+    <div className="container my-5">
+      <h1 className='text-start'><u>About: -</u></h1>
+
     </div>
   )
 }
---
+++ poc1-news-app/src/components/Categories.js
@@ -0,0 +1,13 @@
+import React from 'react'
+// import { Link } from 'react-router-dom'
+
+function Categories() {
+    return (
+        <div className="container my-5">
+            <h1 className='text-start'><u>Categories: -</u></h1>
+            
+        </div>
+    )
+}
+
+export default Categories
