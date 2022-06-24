86e172ef4bbc0271c7d4b32b7bd61f3ea3d49526
it-dreams <rahulraj.holi888@gmail.com>
2022-06-24 10:18:48 +0530

    nested routing

--- poc1-news-app/src/App.js
+++ poc1-news-app/src/App.js
@@ -24,14 +24,16 @@ function App() {
           <div className='col-lg-9 col-md-8 col-sm-12'>
             <Routes>
               <Route exect path='about' element={<About />} />
-              <Route exect path='category' element={<Categories />} />
-              <Route exect path='/' element={<News key='general' style={{ minHeight: '90vh' }} apiKey={apiKey} newsType={newsType} newsDomain={newsDomain} category='general' country={country} pageSize={pageSize} />}></Route>
+              <Route exect path='category' element={<Categories />}>
+                <Route exect path='/' element={<News key={category} style={{ minHeight: '90vh' }} apiKey={apiKey} newsType={newsType} newsDomain={newsDomain} category={category} country={country} pageSize={pageSize} />}></Route>
+              </Route>
+              {/* <Route exect path='/' element={<News key='general' style={{ minHeight: '90vh' }} apiKey={apiKey} newsType={newsType} newsDomain={newsDomain} category='general' country={country} pageSize={pageSize} />}></Route>
               <Route exect path='/business' element={<News key='business' style={{ minHeight: '90vh' }} apiKey={apiKey} newsType={newsType} newsDomain={newsDomain} category='business' country={country} pageSize={pageSize} />}></Route>
               <Route exect path='/entertainment' element={<News key='entertainment' style={{ minHeight: '90vh' }} apiKey={apiKey} newsType={newsType} newsDomain={newsDomain} category='entertainment' country={country} pageSize={pageSize} />}></Route>
               <Route exect path='/health' element={<News key='health' style={{ minHeight: '90vh' }} apiKey={apiKey} newsType={newsType} newsDomain={newsDomain} category='health' country={country} pageSize={pageSize} />}></Route>
               <Route exect path='/science' element={<News key='science' style={{ minHeight: '90vh' }} apiKey={apiKey} newsType={newsType} newsDomain={newsDomain} category='science' country={country} pageSize={pageSize} />}></Route>
               <Route exect path='/sports' element={<News key='sports' style={{ minHeight: '90vh' }} apiKey={apiKey} newsType={newsType} newsDomain={newsDomain} category='sports' country={country} pageSize={pageSize} />}></Route>
-              <Route exect path='/technology' element={<News key='technology' style={{ minHeight: '90vh' }} apiKey={apiKey} newsType={newsType} newsDomain={newsDomain} category='technology' country={country} pageSize={pageSize} />}></Route>
+              <Route exect path='/technology' element={<News key='technology' style={{ minHeight: '90vh' }} apiKey={apiKey} newsType={newsType} newsDomain={newsDomain} category='technology' country={country} pageSize={pageSize} />}></Route> */}
               <Route exect path='*' element={<PageNotFound />}></Route>
             </Routes>
           </div>
